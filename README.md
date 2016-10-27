1bc0a681a0c91b176c49339c4ed7f71e88fb85b9
Eric Warnke <ericew@gmail.com>
2016-10-27 13:25:17 -0400

    Pretty major rewrites

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ for pass and -1 for failure.
 
 - dnspython3
 - requests
+- joblib
 
-`pip3 install [--user] dnspython3 requests`
+`pip3 install [--user] dnspython3 requests joblib`
 
 ## Tests
 
@@ -20,10 +21,12 @@ Planned tests include
 - ~~ping~~ DONE
 - ~~dns query www.google.com should return an ip~~ DONE 
 - ~~dns query of bad name should return ip of host~~ DONE
+- ~~dns over tcp~~
+- ~~stress testing~~
 - dns query of good name should return the same as
   a known good dns
-- request of http://ip/1.js should show pi-hole 
-  based redult
+- ~~request of http://ip/1.js should show pi-hole 
+  based redult~~
 - admin?
 - ssh based test of whitelisting/blacklisting?
 
@@ -31,10 +34,17 @@ Planned tests include
 ## Running
 
 ```
-./validate_pihole.py --help
-usage: validate_pihole.py [-h] [-q] [--skip-dns-good-site] [--skip-api]
+bash$ ./validate_pihole.py -h
+usage: validate_pihole.py [-h] [-q] [--skip-ping]
+                          [--ping-timeout PING_TIMEOUT] [--skip-dns-good-site]
+                          [--skip-dns-localhost] [--enable-stresstest]
+                          [--stress-count STRESS_COUNT]
+                          [--stress-threads STRESS_THREADS]
                           [--skip-dns-bad-site] [--skip-web-blocked]
-                          [--skip-ping]
+                          [--skip-api] [--skip-api-data] [--skip-dns-self]
+                          [--skip-admin-ok] [--skip-dns-good-tcp]
+                          [--skip-dns-bad-tcp]
+                          [--skip-dns-localhost-localdomain]
                           IP
 
 Remote testing of pi-hole installation
@@ -46,26 +56,53 @@ optional arguments:
   -h, --help            show this help message and exit
   -q, --quiet           Run quietly, only output the error on stderr. Add
                         another and it will only return error codes only
-  --skip-dns-good-site  Skip: Query for www.google.com, should return IP that
-                        is not pihole
-  --skip-api            Skip: Test /admin/api.php to make sure it's responding
-  --skip-dns-bad-site   Skip: Query to see if www.doubleclick.net returns
-                        pihole ip
+  --skip-ping           Skip: Probe existance via ping
+  --ping-timeout PING_TIMEOUT
+                        Default: 30
+  --skip-dns-good-site  Skip: DNS for good sites, should return IP that is not
+                        pihole
+  --skip-dns-localhost  Skip: DNS localhost should not return IP (or
+                        127.0.0.1)
+  --enable-stresstest   Enable: Throw {stress_count} domains at the pihole via
+                        {stress_threads} threads
+  --stress-count STRESS_COUNT
+                        Default: 2000
+  --stress-threads STRESS_THREADS
+                        Default: 50
+  --skip-dns-bad-site   Skip: DNS query for known ad sites
   --skip-web-blocked    Skip: Query a random js from site to see that it's
                         return the static file
-  --skip-ping           Skip: Probe existance via ping
+  --skip-api            Skip: Test /admin/api.php to make sure it's responding
+  --skip-api-data       Skip: Test data dump to make sure it's responding
+                        quickly
+  --skip-dns-self       Skip: DNS pi.hole should return own IP
+  --skip-admin-ok       Skip: Query admin/js/other/app.min.js and make sure
+                        it's reasonable size
+  --skip-dns-good-tcp   Skip: DNS for good sites over tcp
+  --skip-dns-bad-tcp    Skip: DNS query for known ad sites over tcp
+  --skip-dns-localhost-localdomain
+                        Skip: DNS localhost.localdomain should be NXDOMAIN
+
 ```
 
 Example run against a functional pihole
 
 ```
-bash$ ./validate_pihole.py 10.195.42.239 
-Probe existance via ping                           ... PASS 
-Query for www.google.com, should return IP that is not pihole ... PASS www.google.com = 216.58.219.228
-Query to see if www.doubleclick.net returns pihole ip ... PASS 10.195.42.239 == 10.195.42.239
-Query a random js from site to see that it's return the static file ... PASS 
-Test /admin/api.php to make sure it's responding   ... PASS Ad percentage today=36.4
-All tests pass on 10.195.42.239
+bash$ ./validate_pihole.py --enable-stresstest 10.195.42.2
+Probe existance via ping                                                         ... PASS Ping... pong!
+DNS pi.hole should return own IP                                                 ... PASS pi.hole == 10.195.42.2
+DNS for good sites, should return IP that is not pihole                          ... PASS www.google.com www.facebook.com www.yahoo.com GOOD
+DNS for good sites over tcp                                                      ... PASS www.google.com www.facebook.com www.yahoo.com GOOD
+DNS localhost should not return IP (or 127.0.0.1)                                ... PASS pi.hole returning 127.0.0.1 for localhost
+DNS localhost.localdomain should be NXDOMAIN                                     ... PASS NXDOMAIN
+DNS query for known ad sites                                                     ... PASS www.doubleclick.net www.googleadservices.com rad.msn.com ads.google.com ad5.liverail.com BLOCKED
+DNS query for known ad sites over tcp                                            ... PASS www.doubleclick.net www.googleadservices.com rad.msn.com ads.google.com ad5.liverail.com BLOCKED
+Throw 2000 domains at the pihole via 50 threads                                  ... PASS 1988/12 min 0.63ms avg 220.00ms max 6746.62ms std 516.91ms
+Query a random js from site to see that it's return the static file              ... PASS var x = "Pi-hole: A black hole for Internet advertisements."
+Query admin/js/other/app.min.js and make sure it's reasonable size               ... PASS /*! AdminLTE app.js
+Test /admin/api.php to make sure it's responding                                 ... PASS Ad percentage today=4.0
+Test data dump to make sure it's responding quickly                              ... PASS Got 2190 queries
+All tests pass on 10.195.42.2
 bash$ echo $?
 0
 ```
@@ -73,14 +110,22 @@ bash$ echo $?
 Example against centos 6 with broken php
 
 ```
-bash$ ./validate_pihole.py 10.195.42.176 --skip-ping
-Query for www.google.com, should return IP that is not pihole ... PASS www.google.com = 216.58.219.228
-Query to see if www.doubleclick.net returns pihole ip ... PASS 10.195.42.176 == 10.195.42.176
-Query a random js from site to see that it's return the static file ... PASS 
-Test /admin/api.php to make sure it's responding   ... FAIL
+bash$ ./validate_pihole.py --enable-stresstest 10.195.42.126
+Probe existance via ping                                                         ... PASS Ping... pong!
+DNS pi.hole should return own IP                                                 ... PASS pi.hole == 10.195.42.126
+DNS for good sites, should return IP that is not pihole                          ... PASS www.google.com www.facebook.com www.yahoo.com GOOD
+DNS for good sites over tcp                                                      ... PASS www.google.com www.facebook.com www.yahoo.com GOOD
+DNS localhost should not return IP (or 127.0.0.1)                                ... PASS pi.hole returning 127.0.0.1 for localhost
+DNS localhost.localdomain should be NXDOMAIN                                     ... PASS NXDOMAIN
+DNS query for known ad sites                                                     ... PASS www.doubleclick.net www.googleadservices.com rad.msn.com ads.google.com ad5.liverail.com BLOCKED
+DNS query for known ad sites over tcp                                            ... PASS www.doubleclick.net www.googleadservices.com rad.msn.com ads.google.com ad5.liverail.com BLOCKED
+Throw 2000 domains at the pihole via 50 threads                                  ... PASS 1993/7 min 0.69ms avg 208.00ms max 10998.59ms std 512.38ms
+Query a random js from site to see that it's return the static file              ... PASS var x = "Pi-hole: A black hole for Internet advertisements."
+Query admin/js/other/app.min.js and make sure it's reasonable size               ... PASS /*! AdminLTE app.js
+Test /admin/api.php to make sure it's responding                                 ... FAIL
 OUTPUT:
 Get return http status code 500
-Testing failed on 10.195.42.176
+Testing failed on 10.195.42.126
 bash$ echo $?
 255
 ```
--- validate_pihole.py
+++ validate_pihole.py
@@ -1,143 +1,251 @@
 #!/usr/bin/env python3
+'''
 
+Validation script for pi-hole to confirm external operation of all the parts.
+
+Functions named test_* are automatically picked up and
+run automatically based on their sortable value.  The system will
+stop and return -1 on the first failure.
+
+test_value_name are added to the command line with their doc string
+  and an option to skip
+
+opt_test_value_name are added as optional with a command line
+  enable function
+
+
+'''
+
+import inspect
 import subprocess
 import dns.resolver
 import requests
 import argparse
+import random
+from contexttimer import Timer
 from pprint import pprint
 import sys
 
 pidns = dns.resolver.Resolver()
-pidns.lifetime = 10
 
-def dns_responds( server, name, rtype='A' ):
-    """ This return true if any valid records are returned
+##################
+# some variables
+
+hosts_spam = [
+        'www.doubleclick.net',
+        'www.googleadservices.com',
+        'rad.msn.com',
+        'ads.google.com',
+        'ad5.liverail.com',
+        ]
+
+hosts_ham = [
+        'www.google.com',
+        'www.facebook.com',
+        'www.yahoo.com',
+    ]
+
+def dns_responds( server, name, tcp=False ):
+    """
+    This return true if any valid records are returned
     """
 
     try:
-        records = server.query( name, rtype )
+        records = server.query( name, tcp=tcp )
         if records:
             return True, "DNS is Alive"
     except dns.exception.DNSException:
         return False, "Query Failed"
-  
 
-def dns_NXDOMAIN( server, name, rtype='A' ):
-    '''Return true if server replies with NXDOMAIN
+def dns_any( server, name, rtype="A", tcp=False ):
+    '''
+    Returns number of records. Timeouts 
+    should throw an exception.  Useful for
+    stress testing the DNS.
+    '''  
+
+    try:
+        records = server.query( name, tcp=tcp )
+        if records: return len(records)
+    except dns.resolver.NXDOMAIN:
+        return 0
+    except dns.resolver.NoAnswer:
+        return 0
+    except dns.resolver.NoNameservers:
+        return 0
+
+def dns_NXDOMAIN( server, name, tcp=False ):
+    '''
+    Return true if server replies with NXDOMAIN
     '''
 
     try:
-        records = server.query( name, rtype )
+        records = server.query( name, tcp=tcp )
         if records:
-            return False,  "%s: %s != NXDOMAIN == %s"%(rtype, name, records[0])
+            return False,  "%s != NXDOMAIN == %s"%(name, records[0])
         else:
             return True, "No records for %s"%name
     except dns.resolver.NXDOMAIN:
-        return True, "%s: %s == NXDOMAIN"%(rtype, name)
+        return True, "%s == NXDOMAIN"%( name)
     except dns.exception.DNSException:
         pass
     return True, "Query failure"
 
-def dns_equals( server, name, match, rtype='A' ):
-    ''' Returns true is server replies equals match
+def dns_equals( server, name, match, tcp=False ):
+    ''' 
+    Returns true is server replies equals match
     '''
 
     try:
-        records = server.query( name, rtype )
+        records = server.query( name, tcp=tcp )
         for rec in records:
             if rec.to_text() == match:
-                return True, "%s: %s == %s"%(rtype, name, match)
-        return False, "%s: %s != %s"%(rtype, name, match)
+                return True, "%s == %s"%(name, match)
+        return False, "%s != %s"%(name, match)
     except dns.exception.DNSException:
         return False, "Query failure"
 
-def dns_not_equals( server, name, match, rtype='A' ):
-    ''' Returns true is server replies does not equals match
+def dns_not_equals( server, name, match, tcp=False ):
+    ''' 
+    Returns true is server replies does not equals match
     '''
 
     try:
-        records = server.query( name, rtype )
+        records = server.query( name, tcp=tcp )
         for rec in records:
             if rec.to_text() == match:
-                return False, "%s: %s == %s"%(rtype, name, match)
-        return True, "%s: %s != %s"%(rtype, name, match)
+                return False, "(%s)%s == %s"%(name, rec.to_text(), match)
+        return True, "(%s)%s != %s"%(name, records[0], match)
     except dns.exception.DNSException:
         return False, "Query failure"
 
-def opt_test_01_ping(args):
-    """Probe existance via ping
-
+def a1_test_ping(IP=None, ping_timeout=30, **kwargs):
+    """
+    Probe existance via ping
     """
 
-    ping = subprocess.Popen(
-        ["ping", "-c", "4", args.IP],
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE
-    )
+    for i in range(ping_timeout):
+        ping = subprocess.Popen(
+            ["ping", "-c", "1", IP],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+        )
 
-    out, error = ping.communicate()
-    if not ping.returncode:
-        return True, "" 
-    else:
-        return False, "Host not pingable"
+        out, error = ping.communicate()
+        if not ping.returncode:
+            return True, "Ping... pong!" 
+    return False, "Host not pingable"
 
-def test_04_dns_self(args):
-    """DNS pi.hole should return own IP
+def a4_test_dns_self(IP=None, **kwargs):
     """
-    (good, result) =  dns_equals(pidns, 'pi.hole', args.IP)
+    DNS pi.hole should return own IP
+    """
+    (good, result) =  dns_equals(pidns, 'pi.hole', IP)
     if good:
         return True, result
     else:
         return False, "%s: Check to see that this is a pi-hole and not firewalled"%result
 
 
-def test_05_dns_good_site(args):
-    """DNS for www.google.com, should return IP that is not pihole
+def a5_test_dns_good_site(IP=None, **kwargs):
     """
-    (good, result) =  dns_not_equals(pidns, 'www.google.com', args.IP)
-    if good:
-        return True, result
-    else:
-        return False, "%s: check blacklists"%result
+    DNS for good sites, should return IP that is not pihole
+    """
+    for host in hosts_ham:
+        (good, result) =  dns_not_equals(pidns, host, IP)
+        if not good:
+            return False, "%s: check blacklists"%result
+    return True, "%s GOOD"%(" ".join(hosts_ham))
 
-def test_08_dns_localhost(args):
-    """DNS localhost should not return IP (or 127.0.0.1)
+def a5_test_dns_good_tcp(IP=None, **kwargs):
+    """
+    DNS for good sites over tcp
+    """
+    for host in hosts_ham:
+        (good, result) =  dns_not_equals(pidns, host, IP, tcp=True)
+        if not good:
+            return False, "%s: check blacklists and firewall 53/tcp"%result
+    return True, "%s GOOD"%(" ".join(hosts_ham))
+
+def a8_test_dns_localhost(**kwargs):
+    """
+    DNS localhost should not return IP (or 127.0.0.1)
     """
     ( good, result ) = dns_NXDOMAIN( pidns, 'localhost.')
-    ( good2, result2 ) = dns_equals( pidns, 'localhost.', '127.0.0.1')
     if not good:
+        ( good2, result2 ) = dns_equals( pidns, 'localhost.', '127.0.0.1')
         if good2:
             return True, "pi.hole returning 127.0.0.1 for localhost"
         else:
             return False, "%s: localhost should be NXDOMAIN"%result
     return True, "NXDOMAIN"
 
-def test_09_dns_localhost_localdomain(args):
-    '''DNS localhost.localdomain should be NXDOMAIN
+def a9_test_dns_localhost_localdomain(**kwargs):
+    '''
+    DNS localhost.localdomain should be NXDOMAIN
     '''
     ( good, result ) = dns_NXDOMAIN( pidns, 'localhost.localdomain.')
     if not good:
-        return False, "%s: should whitelist"%result
+        return False, "%s: execute `pihole -w localhost.localdomain` to clear error"%result
     return True, "NXDOMAIN"
 
-def test_10_dns_bad_site(args):
-    """DNS query for known ad sites
+def b1_test_dns_bad_site(IP=None, **kwargs):
     """
-    sites = [
-        'www.doubleclick.net',
-        'www.googleadservices.com',
-        ]
-    for site in sites:
-        ( good, result ) = dns_equals( pidns, site, args.IP )
+    DNS query for known ad sites
+    """
+    for site in hosts_spam:
+        ( good, result ) = dns_equals( pidns, site, IP )
         if not good:
             return False, "%s: Are blacklists setup?"%result
-    return True, "Returning pi.hole IP"
+    return True, "%s BLOCKED"%(" ".join(hosts_spam))
 
-def test_50_web_blocked(args):
-    """Query a random js from site to see that it's return the static file
+def b1_test_dns_bad_tcp(IP=None, **kwargs):
+    """
+    DNS query for known ad sites over tcp
+    """
+    for site in hosts_spam:
+        ( good, result ) = dns_equals( pidns, site, IP, tcp=False )
+        if not good:
+            return False, "%s: Are blacklists setup and firewall open?"%result
+    return True, "%s BLOCKED"%(" ".join(hosts_spam))
+
+def dns_stress(IP,host):
+    mydns = dns.resolver.Resolver()
+    mydns.nameservers=[IP]
+    with Timer() as t:
+        try:
+            dns_any(mydns, host, tcp=(random.random())>0.5)
+            return ('good', t.elapsed)
+        except:        
+            return ('bad', t.elapsed)
+
+def h0_opt_test_stresstest(IP=None, stress_count=2000, stress_threads=50, **kwargs):
+    '''
+    Throw {stress_count} domains at the pihole via {stress_threads} threads
+    '''
+
+    from joblib import Parallel, delayed
+    
+    top_array = open('topsites.txt').read().splitlines()
+    random.shuffle(top_array)
+   
+    results = Parallel(n_jobs=stress_threads, backend='threading' )(delayed(dns_stress)(IP, site) for site in top_array[:stress_count])
+    good = sum( 1 for (a,b) in results if a == 'good' )
+    numbers = [ b for (a,b) in results if a == 'good' ]
+    bad = sum( 1 for (a,b) in results if a == 'bad' )
+    vmin = min(numbers)*1000
+    vmax = max(numbers)*1000
+    vavg = sum(numbers)*1000//len(numbers)
+    vstd = (sum(((n*1000) - vavg) ** 2 for n in numbers) / len(numbers)) ** .5
+
+    return (good/bad)>0.05, "{good}/{bad} min {vmin:.2f}ms avg {vavg:.2f}ms max {vmax:.2f}ms std {vstd:.2f}ms".format(**locals())
+
+def m0_test_web_blocked(IP=None, **kwargs):
+    """
+    Query a random js from site to see that it's return the static file
     """
     try:
-        r = requests.get("http://%s/1.js"%(args.IP), timeout=5.0)
+        r = requests.get("http://%s/1.js"%(IP), timeout=5.0)
         if not r.status_code == requests.codes.ok:
             return False, "Got return http status code %i"%r.status_code 
         if 'var x = "Pi-hole: A black hole for Internet advertisements."' in r.text:
@@ -147,11 +255,12 @@ def test_50_web_blocked(args):
     except:
         return False, "Error/Timeout in http request, make sure web server is operational and not firewalled"
 
-def test_51_admin_ok(args):
-    """Query admin/js/other/app.min.js and make sure it's reasonable size
+def m1_test_admin_ok(IP=None, **kwargs):
+    """
+    Query admin/js/other/app.min.js and make sure it's reasonable size
     """
     try:
-        r = requests.get("http://%s/admin/js/other/app.min.js"%(args.IP), timeout=5.0)
+        r = requests.get("http://%s/admin/js/other/app.min.js"%(IP), timeout=5.0)
         if not r.status_code == requests.codes.ok:
             return ( False, "Got return http status code %i"%r.status_code )
         if len(r.text)>300:
@@ -161,11 +270,12 @@ def test_51_admin_ok(args):
     except:
         return False, "Error/Timeout in http request, make sure web server is operational and not firewalled"
 
-def test_55_api(args):
-    """Test /admin/api.php to make sure it's responding
+def m5_test_api(IP=None, **kwargs):
+    """
+    Test /admin/api.php to make sure it's responding
     """
     try:
-        r = requests.get("http://%s/admin/api.php?summary"%(args.IP), timeout=5.0, headers={'host':'pi.hole'})
+        r = requests.get("http://%s/admin/api.php?summary"%(IP), timeout=2.0, headers={'host':'pi.hole'})
         if not r.status_code == requests.codes.ok:
             return False, "Get return http status code %i"%r.status_code 
         if r.json():
@@ -175,18 +285,43 @@ def test_55_api(args):
     except:
         return  False, "Error/Timeout/json failure in http request, make sure web server is operational and not firewalled" 
 
-def run_tests(args):
-    for name,func in sorted( (name,func) for name, func in globals().items() if 'test_' in name and not args.__dict__[name]):
-        test_str = func.__doc__.strip()
-        if not args.quiet:
+def m6_test_api_data(IP=None, **kwargs):
+    """
+    Test data dump to make sure it's responding quickly
+    """
+    try:
+        r = requests.get("http://%s/admin/api.php?getAllQueries"%(IP), timeout=5.0, headers={'host':'pi.hole'})
+        if not r.status_code == requests.codes.ok:
+            return False, "Get return http status code %i"%r.status_code 
+        if r.json():
+            return True, "Got %i queries"%len(r.json()['data'])
+        else:
+            return  False, "Malformed json %s"%r.text 
+    except:
+        return  False, "Error/Timeout/json failure in http request, make sure web server is operational and not firewalled" 
+
+
+
+
+#############################################################################################################################
+# Functions below do not need to be changed when adding or removing tests
+
+def run_tests(**kwargs):
+    """
+    This function does the bulk of the processing, walking the list of functions in the module
+    and running them if they have not been excluded
+    """
+    for name,func in sorted( (name,func) for name, func in globals().items() if 'test_' in name and not kwargs[name]):
+        test_str = inspect.getdoc(func).format(**kwargs)
+        if not kwargs['quiet']:
             print("%-80s ... "%test_str, end="", flush=True)
-        (good, out ) = func(args)
-        if good and not args.quiet:
+        (good, out ) = func(**kwargs)
+        if good and not kwargs['quiet']:
             print("PASS %s"%out)
         if not good:
-            if args.quiet: print("%s "%test_str, end="")
+            if kwargs['quiet']: print("%s "%test_str, end="")
             print("FAIL")
-            if out and not args.quiet:
+            if out and not kwargs['quiet']:
                 print("OUTPUT:\n%s"%(out))
             return False
     return True
@@ -195,17 +330,25 @@ def main():
     parser = argparse.ArgumentParser(description="Remote testing of pi-hole installation")
     parser.add_argument('-q','--quiet',action='count',help="Run quietly, only output the error on stderr. Add another and it will only return error codes only")
     parser.add_argument('IP', help="IP address to test")
-    for name,func in ( (name,func) for name,func in globals().items() if 'test_' in name):
+    for name,func in [ (name,func) for name,func in globals().items() if 'test_' in name]:
         if 'opt_' in name:
             alt = "--enable-%s"%(name.split('_', 3)[3].replace('_','-'))
-            parser.add_argument(alt, action='store_false', dest=name, help="Enable: %s"%(func.__doc__.strip()))
+            parser.add_argument(alt, action='store_false', dest=name, help="Enable: %s"%(inspect.getdoc(func)))
         else:
             alt = "--skip-%s"%(name.split('_', 2)[2].replace('_','-'))
-            parser.add_argument(alt, action='store_true', dest=name, help="Skip: %s"%(func.__doc__.strip()))
+            parser.add_argument(alt, action='store_true', dest=name, help="Skip: %s"%(inspect.getdoc(func)))
+        argspec = inspect.getargspec(func)
+        for ( number, name ) in enumerate(argspec.args):
+            if not name == "IP":
+                alt = "--%s"%(name.replace('_','-'))
+                if isinstance(argspec.defaults[number], int):
+                    parser.add_argument(alt, action='store', type=int, dest=name, help="Default: {}".format(argspec.defaults[number]), default=argspec.defaults[number])
+                else:
+                    parser.add_argument(alt, action='store', dest=name, default=argspec.defaults[number])
     args = parser.parse_args()
     
     pidns.nameservers = [ args.IP ]
-    if run_tests(args):
+    if run_tests(**args.__dict__):
         if not args.quiet: print("All tests pass on %s"%(args.IP))
     else:
         print("Testing failed on %s"%(args.IP))
